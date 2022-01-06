be6ba63c0d69496b2966051ae323f9ee20a33933
Cuong Nguyen Manh <cuongnm4215@gmail.com>
2022-01-06 16:58:34 +0700

    Update fake call APIs

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,20 @@
 import Layout from '../../components/layout';
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ post }) {
+  return <Layout>
+    <div className="post">
+      <div className="post__header">
+        <div className="post__id">{post.id}.</div>
+        <div className="post__title">{post.title}</div>
+      </div>
+      <div className="post__desc">{post.body}</div>
+    </div>
+  </Layout>;
 }
 
-export async function getStaticPaths() {}
+export async function getServerSideProps({ params }) {
+  const { id } = params;
+  const post = await (await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)).json();
+
+  return { props: { post } };
+}
