b8d4a067a9be79b1788eb96af8893bbae6ab42b4
Cuong Nguyen Manh <cuongnm4215@gmail.com>
2022-01-07 17:05:41 +0700

    Update UI

--- components/layout.js
+++ components/layout.js
@@ -1,42 +1,28 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import styles from './layout.module.scss';
-import utilStyles from '../styles/utils.module.scss';
-
-const name = 'Nguyen Manh Cuong';
-export const siteTitle = 'Next.js';
 
 export default function Layout({ children, home }) {
+  const router = useRouter();
+  const { pathname } = router;
+  console.log({ pathname });
+
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <title>{siteTitle}</title>
-        <meta name="og:title" content={siteTitle} />
+        <meta name="description" content="Learn how to build a personal website using Next.js" />
+        <title>Next.js</title>
+        <meta name="og:title" content="Next.js" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
-        <Image
-          priority
-          src="/images/profile.jpg"
-          className={utilStyles.borderCircle}
-          height={144}
-          width={144}
-          alt={name}
-        />
-        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+        <div className={styles.navigator}>
+          <Link href="/" className={pathname == '/' ? styles.link__active : ''}>Home</Link>
+          <Link href="/todos" className={pathname == '/todos' ? styles.link__active : ''}>Todo</Link>
+        </div>
       </header>
       <main>{children}</main>
       {!home && (
