2e2b173a8e764b817c758b8c8e74959f39ba0a6e
Cuong Nguyen Manh <cuongnm4215@gmail.com>
2022-01-09 18:52:25 +0700

    Update CSS

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,32 +1,34 @@
 import Layout from '../../components/layout';
 
 export default function Post({ post, comments }) {
-  return <Layout>
-    <div className="post">
-      <div className="post__header">
-        <div className="post__id">{post.id}.</div>
-        <div className="post__title">{post.title}</div>
-      </div>
-      <div className="post__desc">{post.body}</div>
-    </div>
-    <div className="comments">
-      <p className="comments__title">Comments:</p>
-      { comments.map((comment) => (
-        <div className="comment" key={comment.id}>
-          <div className="comment__left">
-            <div className="comment__id">{comment.id}</div>
-          </div>
-          <div className="comment__right">
-            <div className="comment__author">
-              <span className="author__name">Fullname: {comment.name}</span>
-              <span className="author__email">Email: {comment.email}</span>
-            </div>
-            <div className="comment__content">{comment.body}</div>
-          </div>
+  return (
+    <Layout>
+      <div className="post">
+        <div className="post__header">
+          <div className="post__id">{post.id}.</div>
+          <div className="post__title">{post.title}</div>
         </div>
-      )) }
-    </div>
-  </Layout>;
+        <div className="post__desc">{post.body}</div>
+      </div>
+      <div className="comments">
+        <p className="comments__title">Comments:</p>
+        {comments.map((comment) => (
+          <div className="comment" key={comment.id}>
+            <div className="comment__left">
+              <div className="comment__id">{comment.id}</div>
+            </div>
+            <div className="comment__right">
+              <div className="comment__author">
+                <span className="author__name">Fullname: {comment.name}</span>
+                <span className="author__email">Email: {comment.email}</span>
+              </div>
+              <div className="comment__content">{comment.body}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 }
 
 export async function getServerSideProps({ params }) {
